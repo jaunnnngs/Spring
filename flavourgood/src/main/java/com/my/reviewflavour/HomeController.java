0e05d51871742151544f9ea6b9146973c2b5fb4f
minyoung0 <81130005+minyoung0@users.noreply.github.com>
2021-09-22 17:37:08 +0900

    새로운 프로젝트

---
+++ flavourgood/src/main/java/com/my/reviewflavour/HomeController.java
@@ -0,0 +1,66 @@
+package com.my.reviewflavour;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/map", method = { RequestMethod.POST,RequestMethod.GET})
+	public String map(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "map";
+	}
+	@RequestMapping(value = "/evaluate", method = { RequestMethod.POST,RequestMethod.GET})
+	public String evaluate(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "evaluate";
+	}
+	
+}
--- flavourgood/src/main/webapp/WEB-INF/views/home.jsp
+++ flavourgood/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,13 @@
- <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
 
 </head>
 <body>
-<form action="loginchk" method="post">
+<form action="map" method="post">
 <div id="root">
 	<div class="container">
 				<div><h3>로그인</h3></div>
