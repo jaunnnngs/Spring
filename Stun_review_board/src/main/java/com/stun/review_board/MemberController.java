bb2ece42288c0f9201a7b60d6ca1c18d94fd0757
jaunnnngs <81130206+jaunnnngs@users.noreply.github.com>
2021-09-22 14:22:22 +0900

    update

--- Stun_review_board/src/main/java/com/stun/review_board/MemberController.java
+++ Stun_review_board/src/main/java/com/stun/review_board/MemberController.java
@@ -1,5 +1,30 @@
 package com.stun.review_board;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.stun.review_board.dao.MemberDAO;
+import com.stun.review_board.dto.MemberDTO;
+
+@Controller
 public class MemberController {
 
+	@Autowired
+	MemberDAO dao;
+	
+	@RequestMapping(value="/logincheck", method = RequestMethod.POST)
+	public String logincheck(MemberDTO dto, HttpSession session) {
+		dto= dao.logincheck(dto);
+		if(dto != null) {
+			session.setAttribute("dto", dto);
+			return "chart";
+		}
+		else {
+			return "redirect:/?text=loginfail";
+		}
+	}
 }
