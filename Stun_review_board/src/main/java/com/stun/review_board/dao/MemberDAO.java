3ff6ec73cea773300645f29d9668fbef5ffa14c6
jaunnnngs <81130206+jaunnnngs@users.noreply.github.com>
2021-09-26 13:23:27 +0900

    update

--- Stun_review_board/src/main/java/com/stun/review_board/dao/MemberDAO.java
+++ Stun_review_board/src/main/java/com/stun/review_board/dao/MemberDAO.java
@@ -1,5 +1,22 @@
 package com.stun.review_board.dao;
 
-public class MemberDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.stun.review_board.dto.MemberDto;
+
+@Repository
+public class MemberDao {
+
+
+	@Autowired
+	SqlSession sqlsession;
+	
+	public MemberDto chklogin(MemberDto dto) {
+		dto=sqlsession.selectOne("member.loginchk",dto);
+		return dto;
+	}
+	
+	
 }
